1f54362034fe1e792fd64664f7117465279b8d27
Matija Maksimović <116630740+MatijaMax@users.noreply.github.com>
2024-03-11 00:56:27 +0100

    Student test init

---
+++ encounters-service/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encounters/handler"
+	"encounters/model"
+	"encounters/repo"
+	"encounters/service"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func initDB() *gorm.DB {
+	dsn := "host=localhost user=postgres password=super dbname=gormen port=5432 sslmode=disable"
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		print(err)
+		return nil
+	}
+
+	database.AutoMigrate(&model.Student{})
+	return database
+}
+
+func main() {
+	database := initDB()
+	if database == nil {
+		print("FAILED TO CONNECT TO DB")
+		return
+	}
+	studentRepo := &repo.StudentRepository{DatabaseConnection: database}
+	studentService := &service.StudentService{StudentRepo: studentRepo}
+	studentHandler := &handler.StudentHandler{StudentService: studentService}
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/students/{id}", studentHandler.Get).Methods("GET")
+	router.HandleFunc("/students", studentHandler.Create).Methods("POST")
+
+	// Set up CORS middleware
+	allowedHeaders := handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"})
+	allowedOrigins := handlers.AllowedOrigins([]string{"*"})
+	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
+
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static")))
+	println("Server starting")
+	log.Fatal(http.ListenAndServe(":8081", handlers.CORS(allowedHeaders, allowedOrigins, allowedMethods)(router)))
+}
